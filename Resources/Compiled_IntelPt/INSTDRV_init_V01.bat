510735c97bfda4fbb409652278cf6029fb1aeed7
Diego Provinciani <diegoprovinciani@gmail.com>
2018-04-30 20:28:17 -0300

    Updating install driver bat scripts

--- Resources/Compiled_IntelPt/INSTDRV_init_V01.bat
+++ Resources/Compiled_IntelPt/INSTDRV_init_V01.bat
@@ -1,1 +1,1 @@
-C:\Development\Tesis\Resources\Compiled_IntelPt\INSTDRV.EXE C:\Development\Tesis\Resources\Compiled_IntelPt\V01\WindowsPtDriver.sys
+INSTDRV.EXE V01\WindowsPtDriver.sys
--- Resources/Compiled_IntelPt/INSTDRV_init_V02.bat
+++ Resources/Compiled_IntelPt/INSTDRV_init_V02.bat
@@ -1,1 +1,1 @@
-C:\Development\Tesis\Resources\Compiled_IntelPt\INSTDRV.EXE C:\Development\Tesis\Resources\Compiled_IntelPt\V02\WindowsPtDriver.sys
+INSTDRV.EXE V02\WindowsPtDriver.sys
